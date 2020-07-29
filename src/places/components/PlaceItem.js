e32fe75f41f807f144dad7c1280c299bc1cc40d3
3113v3n <sydneyreezy+github@gmail.com>
2020-07-29 14:33:06 +0300

    Dynamic Modal Setup

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PlaceItem.css";
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 const PlaceItem = (props) => {
+  const [showMap, setShowMap] = useState(false);
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title} />
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass={"place-item__model-content"}
+        footerClass={"plcae-item__modal-actions"}
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <h2>The Map</h2>
         </div>
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
-          <button> VIEW ON MAP</button>
-          <button> EDIT</button>
-          <button> DELETE</button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              {" "}
+              VIEW ON MAP
+            </Button>
+            <Button to={`/paces/${props.id}`}> EDIT</Button>
+            <Button danger> DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 };
 
-export default PlaceItem
+export default PlaceItem;
